9a8f55c723c6bbcf102bc34cf0d7f0db07882da1
DavidAdewale <adewaledavidola@gmail.com>
2025-08-02 15:17:17 +0100

    updated video

--- src/pages/index/WhyWeExist.jsx
+++ src/pages/index/WhyWeExist.jsx
@@ -12,7 +12,7 @@ function WhyWeExist() {
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/tnltNkowUqk?si=pVUGDEA9Y9A8yNf9"
+            src="https://www.youtube.com/embed/QzLmkPjM84Y?si=Bkm3--PiLeOb_ylG"
             title="Why Moneda Exists"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin"
