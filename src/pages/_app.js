deda0a9f27261cdeffd398c7c83d92418f5daed7
DavidAdewale <adewaledavidola@gmail.com>
2026-02-11 16:01:08 +0100

    update font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,18 +3,35 @@ import { MenuTogglerProvider } from '@/context/MenuToggleContext';
 import { SmoothScrollProvider } from '@/context/SmoothScrollContext';
 import '@/styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Poppins } from 'next/font/google';
 import { Toaster } from 'react-hot-toast';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps }) {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <SmoothScrollProvider>
-        <MenuTogglerProvider>
-          <Component {...pageProps} />
-          <ResponsiveToaster mobileBreakpoint={1200} />
-        </MenuTogglerProvider>
-      </SmoothScrollProvider>
-    </QueryClientProvider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
+      <QueryClientProvider client={queryClient}>
+        <SmoothScrollProvider>
+          <MenuTogglerProvider>
+            <main className={poppins.className}>
+              <Component {...pageProps} />
+              <ResponsiveToaster mobileBreakpoint={1200} />
+            </main>
+          </MenuTogglerProvider>
+        </SmoothScrollProvider>
+      </QueryClientProvider>
+    </>
   );
 }
